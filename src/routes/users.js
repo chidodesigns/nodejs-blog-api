635f1467666b9abe669fde0ac981f1ca9fdcadd2
chidodesigns <chidoukaigwe@gmail.com>
2021-12-25 15:27:58 +0000

    updated user delete route

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,30 +12,5 @@ router.get("/:id", UsersController.GetUserById)
 router.put("/:id", UsersController.UpdateUserById)
 
 //  Delete User
-// router.delete("/:id", async(req, res) => {
-//     if (req.body.userId === req.params.id) {
-//         const user = await User.findById(req.params.id)
-//         if (user) {
-
-//             try {
-//                 await Post.deleteMany({ username: user.username })
-//                 await User.findByIdAndDelete(req.params.id)
-//                 res.status(200).json("User has been deleted...")
-//             } catch (error) {
-//                 res.status(500).json(error);
-//             }
-
-
-//         } else {
-//             res.status(404).json("User not found")
-//         }
-
-
-
-//     } else {
-//         res.status(401).json("You can delete only your account")
-//     }
-// });
-
 router.delete("/:id", UsersController.UserDeleteAccount)
 module.exports = router;
